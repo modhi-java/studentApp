bf6b74f2b9a11e2417f29a71c63247eefaacdd23
Development <islamhaker@gmail.com>
2018-04-04 22:59:07 +0300

    111

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,30 +4,39 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { ;
 
         System.out.println("---------------- بداية عملية الجمع----------------");
+        System.out.println("نتيجة جمع الرقمين تساوي " + getSum());
+        System.out.println("----------------نهاية عملية الجمع----------------");
+
+        System.out.println("---------------- بداية عملية الطرح----------------");
+        System.out.println("نتيجة طرح الرقمين تساوي " + getSubtract());
+        System.out.println("----------------نهاية عملية الطرح----------------");
+
+    }
+
+    public static double getSum(){
+        Scanner input = new Scanner(System.in);
+
         System.out.println("ادخل الرقم الأ,ل:");
         double num1 = input.nextDouble();
 
         System.out.println("ادخل الرقم الثاني:");
         double num2 = input.nextDouble();
 
-        System.out.println("نتيجة جمع الرقمين تساوي " + (num1 + num2));
-        System.out.println("----------------نهاية عملية الجمع----------------");
+        return num1 + num2;
+    }
 
-        System.out.println("---------------- بداية عملية الطرح----------------");
+    public static double getSubtract(){
+        Scanner input = new Scanner(System.in);
+
         System.out.println("ادخل الرقم الأ,ل:");
-        num1 = input.nextDouble();
+        double num1 = input.nextDouble();
 
         System.out.println("ادخل الرقم الثاني:");
-        num2 = input.nextDouble();
+        double num2 = input.nextDouble();
 
-
-        System.out.println("نتيجة طرح الرقمين تساوي " + (num1 - num2));
-        System.out.println("----------------نهاية عملية الطرح----------------");
-
+        return num1 - num2;
     }
 }
